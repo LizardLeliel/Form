f9b47ba8bdd6605500314960dec39db104b30cf8
Leliel <shadow.shade.shine@gmail.com>
2017-12-03 04:24:33 +0000

    Removed test.h and test.c (testing can be done by executing files)

--- src/main.c
+++ src/main.c
@@ -4,26 +4,15 @@
 #include "lex.yy.h"
 #include "build.h"
 #include "runtime.h"
-//#include "test.c"
 
 extern void initializeYYLEXProgramBuilder();
 extern program_context_t finishYYLEXBuild();
 
-
 int main(int argc, char** argv)
 {
 
-//#define test
-
     initializeYYLEXProgramBuilder();
 
-#ifdef test
-//#include "test.h"
-    //runChecks();
-
-#endif
-
-#ifndef test
     if (argc >= 2)
     {   
         yyin = fopen(argv[1], "r");
@@ -40,9 +29,6 @@ int main(int argc, char** argv)
         printf("Please provide a file\n");
         return 0;
     }
-#endif
-#undef test
 
     return 0;
 }
-
